03a02d71275f783f3923de7c5498d8463cc21149
mukando0115 <abcdef0347@gamil.com>
2024-09-01 05:44:04 +0900

    test server for Frontend

--- app.js
+++ app.js
@@ -15,9 +15,25 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    console.log(req.body);
+    console.log('로그인 정보', req.body);
     res.send(res.data);
 });
+
+app.post('/signUp', (req, res) => {
+    const koreaTimeDiff = 9 * 60 * 60 * 1000;
+    const user_birthdate = new Date(req.body.user_birthDate)+koreaTimeDiff;
+     
+    console.log('회원가입 정보', req.body);
+    console.log('보간된 시간', user_birthdate);
+    res.send(res.data);
+});
+
+app.get(`/signUp/:userId`, (req, res) => {
+    let userId = req.params;
+    console.log(userId);
+    //쿼리 부분
+    res.send(false);
+})
 
 app.listen(port, ()=> {
     console.log(`${port}에서 대기중~~~`);
